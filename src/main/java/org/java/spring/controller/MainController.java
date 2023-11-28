e9705d35253d2e6e75daca905f2f255522d8e92c
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-28 14:08:18 +0100

    step 1 completato

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -1,5 +1,22 @@
 package org.java.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MainController {
+	
+	@GetMapping("/")
+	
+	public String home(Model model) {
+		final String name= "Giacomo";
+		
+		model.addAttribute("name", name);
+        return "home";
+	}
+	
+	
 
 }
